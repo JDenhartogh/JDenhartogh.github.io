89f7cd494a8902e8915433d7183ed9cac72832c9
Jeremy D <JeremyDenhartogh@gmail.com>
2019-01-11 16:41:43 -0500

    Bio Updated

---
+++ open.js
@@ -0,0 +1,12 @@
+var pic = document.getElementsByClassName("collapsible");
+for (i = 0; i < pic.length; i++) {
+  pic[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
